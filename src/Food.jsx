ac7553f5406cc3e304f21e50ad5605195b546eae
magobryan81 <magobryan81@gmail.com>
2025-02-24 20:53:28 +0800

    add files (Food, Footer, and Header)

---
+++ src/Food.jsx
@@ -0,0 +1,15 @@
+
+function Food() {
+
+    const food1 = "Chicken";
+    const food2 = "Beef";
+    return(
+        <ul>
+            <li>Apple</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </ul>
+    );
+}
+
+export default Food
---
+++ src/Footer.jsx
@@ -0,0 +1,11 @@
+
+function Footer() {
+    return(
+        <footer>
+            <p>This is my new app.</p>
+            <p>&copy; {new Date().getFullYear()}</p>
+        </footer>
+    )
+}
+
+export default Footer
