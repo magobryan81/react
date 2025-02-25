28ff3b444c98636c051d28ce801077c07ab30911
magobryan81 <magobryan81@gmail.com>
2025-02-26 01:02:07 +0800

    add button component and style module

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Food from './Food.jsx';
 import Card from './Card.jsx';
+import Button from './Button.jsx';
 
 function App() {
   return(
     <>
     <Card />
+    <Button />
     <Header />
     <Food />
     <Footer />
---
+++ src/Button.jsx
@@ -0,0 +1,12 @@
+import buttonStyle from './button.module.css'
+
+function Button() {
+
+    return(
+        <button className={buttonStyle.button}>
+        Click Me!
+        </button>
+    );
+}
+
+export default Button;
