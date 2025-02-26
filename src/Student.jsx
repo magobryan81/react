8e846762c11d83816c0598b377b27c2c2aad1645
magobryan81 <magobryan81@gmail.com>
2025-02-27 01:04:50 +0800

    fix the defaultProps and use default parameters instead

--- src/Student.jsx
+++ src/Student.jsx
@@ -1,11 +1,23 @@
 import PropTypes from 'prop-types'
 
-function Student(props) {
+// function Student(props) {
+//     return(
+//         <div>
+//             <p>Name: {props.name}</p>
+//             <p>Age: {props.age}</p>
+//             <p>Student: {props.isStudent ? "Yes" : "No"}</p>
+//         </div>
+//     );
+// }
+
+
+// it is much better to use it
+function Student({ name = "Guest", age = 0, isStudent = false }) {
     return(
         <div>
-            <p>Name: {props.name}</p>
-            <p>Age: {props.age}</p>
-            <p>Student: {props.isStudent ? "Yes" : "No"}</p>
+            <p>Name: {name}</p>
+            <p>Age: {age}</p>
+            <p>Student: {isStudent ? "Yes" : "No"}</p>
         </div>
     );
 }
