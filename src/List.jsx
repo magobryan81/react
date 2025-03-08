c1112c848cb2c5b2c6a8367bc5ce7ccc0fb5c669
magobryan81 <magobryan81@gmail.com>
2025-03-08 21:46:19 +0800

    add sorting and filter

--- src/List.jsx
+++ src/List.jsx
@@ -1,13 +1,23 @@
 
 function List() {
     
-    const fruits = [{id: 1, name: 'apple', calories: 95}, 
+    const fruits = [{id: 1, name: 'apple', calories: 105}, 
                     {id: 2, name: 'orange', calories: 85}, 
                     {id: 3, name: 'banana', calories: 15}, 
                     {id: 4, name: 'durian', calories: 25}];
-    const fruitsList = fruits.map(fruit => <li key={fruit.id}>
-                                            {fruit.name}: &nbsp;
-                                            {fruit.calories}</li>);
+    // fruits.sort((a, b) => a.name.localeCompare(b.name)); alphabetical
+    // fruits.sort((a, b) => b.name.localeCompare(a.name)); reverse alphabetical
+    // fruits.sort((a, b) => a.calories - b.calories); numerical
+    // fruits.sort((a, b) => b.calories - a.calories); reverse numerical
+
+    // const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+    const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
+    
+    const fruitsList = highCalFruits.map(highCalFruits => <li key={highCalFruits.id}>
+                                            {highCalFruits.name}: &nbsp;
+                                            {highCalFruits.calories}</li>);
+
+                                       
 
     return(
 
